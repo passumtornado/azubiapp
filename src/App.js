2b717f0d55d4e79bcde0d3226d8c023c15aaa1ca
passumtornado <39583908+passumtornado@users.noreply.github.com>
2022-02-06 09:39:14 +0000

    new update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Deploying first react app
+         Deploying first react application
         </p>
        
       </header>
